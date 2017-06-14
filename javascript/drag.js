ae122ce636e3aeba7d2b1a6428a908935ba87b5b
simonUrselmann <splursel@avans.nl>
2017-06-14 18:23:05 +0200

    hoi

--- javascript/drag.js
+++ javascript/drag.js
@@ -1,21 +1,32 @@
 function allowDrop(ev) {
     ev.preventDefault();
 }
-
+var pievetodrop = 0;
 function drag(ev,x,y,piece) {
     ev.dataTransfer.setData("text", ev.target.id);
-    field[x][y] = piece;
+    pievetodrop = piece;
 }
 
 function drop(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));
-    console.log(ev.target);
-    console.log(field);
+    var newx = $(ev.target)[0].style.top.toString().replace('%','')/10;
+    var newy = $(ev.target)[0].style.left.toString().replace('%','')/10;
+    //controle voor verplaatsennaar water
+    // if(!((newx == 4 && newy ==2)||(newx == 4 && newy ==3)||(newx == 4 && newy ==3)||(newx == 5 && newy ==2)||(newx == 5 && newy ==3)||(newx == 4 && newy ==6)||(newx == 4 && newy ==7)||(newx == 5 && newy ==6)||(newx == 5 && newy ==7))){
+    //     ev.target.appendChild(document.getElementById(data));
+    //     field[newx][newy] = pievetodrop;
+    // }
+    if(newx>5)
+    {
+        ev.target.appendChild(document.getElementById(data));
+        field[newx][newy] = pievetodrop;
+    }
+   console.log(field);
 
 }
 
 function denyDrop(ev) {
     ev.preventDefault();
 }
+
