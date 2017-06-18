0613166bf2de80be4fea5b8c6128a41598d15a89
rikscholten <rscholte2@avans.nl>
2017-06-18 02:11:19 +0200

    History

--- javascript/History.js
+++ javascript/History.js
@@ -1,10 +1,16 @@
-class gameHistory {
-
-	constructor(){
-		
-	}
-}
+var moveNumber = 0;
 
 socket.on('move', function(move) {
+
+	moveNumber++;
+
 	console.log('Move:', move);
+
+	var div = document.createElement('div');
+
+	var text = document.createElement('p');
+	text.innerHTML= moveNumber + ' Test';
+
+	div.appendChild(text);
+	$('#history').append(div);
 })
